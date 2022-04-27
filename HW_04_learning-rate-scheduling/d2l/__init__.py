681dee671a89ab5a99854fcd14ef443f112435b3
Yana <nietism@live.com>
2022-04-27 11:35:43 +0800

    Updata HW_04

---
+++ HW_04_learning-rate-scheduling/d2l/__init__.py
@@ -0,0 +1,9 @@
+"""Saved source code for "Dive into Deep Learing" (https://d2l.ai).
+
+Import d2l by one of the following ways:
+
+from d2l import paddle as d2l  # Use PyTorch as the backend
+
+"""
+
+__version__ = "2.0.0-beta0-paddle_version"
